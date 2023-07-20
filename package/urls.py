6b1c62c329fa2f75b9ceae4a41840df10132ca3f
Arshia-Izadyar <arshiaa104@gmail.com>
2023-07-20 18:23:35 +0330

    added base.css / package list

--- package/urls.py
+++ package/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from package.views import PackageView
+from package.views import PackageView, PackageListView
 
 urlpatterns = [
     path('<int:package_pk>/<int:advertisement_pk>/', PackageView.as_view(), name='package-detail'),
+    path('list/<int:advertisement_pk>/', PackageListView.as_view(), name='package-list'),
 ]
--- package/views.py
+++ package/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
 
 from advertisement.models import Advertisement
 from package.models import Package
@@ -16,3 +17,9 @@ class PackageView(LoginRequiredMixin, View):
         package = Package.objects.get(pk=package_pk)
         advertisement = Advertisement.objects.get(pk=advertisement_pk)
         return render(request, self.template_name, {'package': package, 'advertisement': advertisement})
+
+
+class PackageListView(LoginRequiredMixin, ListView):
+    template_name = 'package/package_list.html'
+    queryset = Package.objects.all()
+    context_object_name = 'packages'
