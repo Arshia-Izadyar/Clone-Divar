7c676cf74fdfb7a53b9e25fe34058774c9f486fc
Arshia-Izadyar <arshiaa104@gmail.com>
2023-07-20 15:34:07 +0330

    added form for advertisement

---
+++ advertisement/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+from .models import Advertisement
+
+
+
+class AdvertisementForm(forms.ModelForm):
+    class Meta:
+        model = Advertisement
+        fields = ("title", "description", "image", "price", "category","location")
